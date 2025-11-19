9baffd157598defd1277ab97e883cfaea6750cda
JSkahl <josekahl333@gmail.com>
2025-11-18 22:45:30 -0300

    chore: change catppuccin theme

--- scripts/scripts/dmenu/themes/current.sh
+++ scripts/scripts/dmenu/themes/current.sh
@@ -1,5 +1,5 @@
-DMENU_NB="#0b0f10"     # deep black/green background
-DMENU_NF="#c5d5c5"     # light jade-ish text
-DMENU_SB="#2bbf85"     # jade green accent
-DMENU_SF="#0b0f10"     # selected text
+DMENU_NB="#212335"
+DMENU_NF="#c6d0f5"
+DMENU_SB="#ca9ee6"
+DMENU_SF="#303446"
 DMENU_FONT="JetBrainsMono Nerd Font-15"
