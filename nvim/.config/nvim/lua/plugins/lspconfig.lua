a79bf372759510ca91a574d4a75cd0fd75ed39bb
JSkahl <josekahl333@gmail.com>
2026-02-11 23:38:02 -0300

    changes

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -25,7 +25,16 @@ return {
             vim.lsp.config("html", { capabilities = capabilities })
             vim.lsp.config("cssls", { capabilities = capabilities })
             vim.lsp.config("jsonls", { capabilities = capabilities })
-            vim.lsp.config("lua_ls", { capabilities = capabilities })
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
             vim.lsp.config("ast_grep", { capabilities = capabilities })
             vim.lsp.config("eslint", { capabilities = capabilities })
             vim.lsp.config("pylint", { capabilities = capabilities })
--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,12 +1,3 @@
-local dark_theme = "catppuccin-mocha"
-local light_theme = "catppuccin-latte"
-
 local current_tone = vim.fn.system("gsettings get org.gnome.desktop.interface color-scheme")
 
-if current_tone == "prefer-dark" then
-    vim.cmd.colorscheme(dark_theme)
-end
-
-if current_tone == "prefer-light" then
-    vim.cmd.colorscheme(light_theme)
-end
+vim.cmd.colorscheme("gruvbox")
